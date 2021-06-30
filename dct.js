d0e9c21509ba14bbe553b1a622e1376ecb95c308
CoaPro <srbpro18@gmail.com>
2021-06-30 11:41:07 +0200

    v8

--- dct.js
+++ dct.js
@@ -25,3 +25,21 @@ const demo = () => {
 btnDCT = document.getElementById('btnDCT'); 
 btnDCT.onclick = demo; 
 
+//Hover efekat - JavaScript
+
+btnDCT.addEventListener('mouseover', function(){
+    btnDCT.style.opacity = 0.8;
+});
+
+btnDCT.addEventListener('mouseout', function(){
+    btnDCT.style.opacity = 1;
+});
+
+btnBrisanje.addEventListener('mouseover', function(){
+    btnBrisanje.style.opacity = 0.8;
+});
+
+btnBrisanje.addEventListener('mouseout', function(){
+    btnBrisanje.style.opacity = 1;
+});
+
