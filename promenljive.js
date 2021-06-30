59fc1b44dfcc9134de440098a8c59de4029cd818
CoaPro <srbpro18@gmail.com>
2021-06-30 16:04:07 +0200

    v16 | 30.6.2021.

---
+++ promenljive.js
@@ -0,0 +1,25 @@
+//Globalne promenljive
+
+dimMatr = document.getElementById('inpDimMatr'); 
+//brEl = dimMatr.value;
+
+
+btnMatrica = document.getElementById('btnMatrica'); 
+txtUlaz = document.getElementById('txtUlaz');
+
+btnBrisanje = document.getElementById('btnBrisanje'); 
+
+
+
+infoP1 = document.getElementById('infoP1');
+txtIzlaz = document.getElementById('txtIzlaz'); 
+
+btnDCT = document.getElementById('btnDCT'); 
+
+
+inpBrKoef = document.getElementById('inpBrKoef');
+
+inpMatr = document.getElementById('inpMatr'); 
+
+
+
