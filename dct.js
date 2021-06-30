d25969084e2f18175ed96ace2d380e660a2ae7a0
CoaPro <srbpro18@gmail.com>
2021-06-30 11:13:53 +0200

    v | 30.6.2021.

--- dct.js
+++ dct.js
@@ -4,4 +4,14 @@
 btnBrisanje = document.getElementById('btnBrisanje'); 
 btnBrisanje.style.backgroundColor = 'rgb(229, 43, 80)'; 
 
+//Dan2
+//30.6.2021. Sre. 
+
+txtUlaz = document.getElementById('txtUlaz');
+txtUlaz.value = 'Projekat je u fazi izrade';
+
+txtIzlaz = document.getElementById('txtIzlaz'); 
+txtIzlaz.value = 'Projekat je u fazi izrade'; 
+
+
 
