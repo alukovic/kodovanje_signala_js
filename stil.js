e5bb4034f3ded98145494f85efbf7f051e53b752
CoaPro <srbpro18@gmail.com>
2021-06-30 14:38:05 +0200

    v15 | 30.6.2021.

---
+++ stil.js
@@ -0,0 +1,17 @@
+//Hover efekat - JavaScript
+
+btnDCT.addEventListener('mouseover', function(){
+    btnDCT.style.opacity = 0.8;
+});
+
+btnDCT.addEventListener('mouseout', function(){
+    btnDCT.style.opacity = 1;
+});
+
+btnBrisanje.addEventListener('mouseover', function(){
+    btnBrisanje.style.opacity = 0.8;
+});
+
+btnBrisanje.addEventListener('mouseout', function(){
+    btnBrisanje.style.opacity = 1;
+});
