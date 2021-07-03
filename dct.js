7143278736f7a09c30ad389437fe3e799e448e37
CoaPro <srbpro18@gmail.com>
2021-07-03 18:20:37 +0200

    v3 | 3.7.2021.

--- dct.js
+++ dct.js
@@ -15,7 +15,7 @@ const generisanjeMatrice = () => {
     ulaznaMatrica = new Array(Number(dimMatr.value**2));
 
     for(let i = 0; i < ulaznaMatrica.length; i++){
-        ulaznaMatrica[i] = Number((Math.random()).toFixed(2));
+        ulaznaMatrica[i] = Number((Math.random()*100).toFixed(2));
         //console.log(tabela[i]);
     }
     
@@ -62,42 +62,108 @@ btnDCT.onclick = demo;
 */
 
 const DCT = () => {
+
     //test
     //txtIzlaz.value = ulaznaMatrica; 
     j = Number(), x = Number(), y = Number(), u = Number(), v = Number();
     izlaznaMatrica = new Array(Number(dimMatr.value**2));
-
+    //izlaznaMatrica = []; 
     F = Number(); 
    
-
     Cu = 0;
     Cv = 0; 
+    n = dimMatr.value;
+    m = dimMatr.value;
+    suma = Number(); 
 
-    for(u = 0; u < dimMatr.value; u++){
+    /*
+    Kod #1
+    for(u = 0; u < n; u++){
 
         izlaznaMatrica[u] = ulaznaMatrica[u];
 
-        for(v = 0; v < dimMatr.value; v++){
-            izlaznaMatrica[u][v] = ulaznaMatrica[u][v];
+        for(v = 0; v < m; v++){
+
+            suma = 0;
+
+            for(x = 0; x < dimMatr.value; x++){
+
+                for(y = 0; y < dimMatr.value; y++){
+                    suma += ulaznaMatrica[x][y]*Math.cos(((2*x + 1)*u*Math.PI)/16)*Math.cos(((2*y + 1)*v*Math.PI)/16); 
+                }
+
+            }
+
+            if(u === 0){
+                Cu = Number((1/Math.sqrt(2)).toFixed(2));
+            } else
+            {
+                Cu = 1; 
+            }
+
+            if(Cv === 0){
+                Cv = Number((1/Math.sqrt(2)).toFixed(2));
+            } else {
+                Cv = 1;
+            }
+
+            //izlaznaMatrica[u][v] = ulaznaMatrica[u][v];
+            izlaznaMatrica[u][v] = (1/4)*Cu*Cv*suma; 
+            console.log(izlaznaMatrica[u][v]); 
         }
 
     }
+    */
+ 
     
-    /*
+    
+    //Kod #2
     for(j = 0; j < izlaznaMatrica.length; j++){
-        izlaznaMatrica[j] = Number((ulaznaMatrica[j]).toFixed(0));
-
+        izlaznaMatrica[j] = Number((ulaznaMatrica[j]));
+        
     } 
 
     for(u = 0; u < izlaznaMatrica.length; u++){
-        for(v = 0; v < izlaznaMatrica[u].length; v++){
-            izlaznaMatrica[u][v] = u*v; 
-            //tabelaS += tabela[red][kol] + '\n';
-    
-        }
-    }
-    */
+        
+        console.log('testA'); 
 
+        for(v = 0; v < izlaznaMatrica[u].length; v++){
+            //izlaznaMatrica[u][v] = u*v; 
+            //tabelaS += tabela[red][kol] + '\n'; 
+
+            suma = 0;
+            console.log('testB');
+
+            for(x = 0; x < dimMatr.value; x++){
+
+                for(y = 0; y < dimMatr.value; y++){
+                    suma += ulaznaMatrica[x][y]*Math.cos(((2*x + 1)*u*Math.PI)/16)*Math.cos(((2*y + 1)*v*Math.PI)/16); 
+                    console.log('test'); 
+                    console.log('test: ' + ulaznaMatrica[x][y]); 
+                }
+
+            }
+
+            if(u === 0){
+                Cu = Number((1/Math.sqrt(2)).toFixed(2));
+            } else
+            {
+                Cu = 1; 
+            }
+
+            if(Cv === 0){
+                Cv = Number((1/Math.sqrt(2)).toFixed(2));
+            } else {
+                Cv = 1;
+            }
+
+            //izlaznaMatrica[u][v] = ulaznaMatrica[u][v];
+            izlaznaMatrica[u][v] = (1/4)*Cu*Cv*suma; 
+            console.log(izlaznaMatrica[u][v]); 
+        }
+
+    }
+    
     //za matricu 2x2
     //[0][0] [0][1] [1][0] [1][1]
 
